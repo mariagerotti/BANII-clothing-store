c01cddd90b69e6c7b9bd8fe3786688691a33fffc
mariagerotti <mariaeduarda.gerotti@gmail.com>
2026-09-25 15:58:46 -0300

    fix: insert maven in project

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/loja_roupas";
     private static final String USUARIO = "postgres";
-    private static final String SENHA = "postgres";
+    private static final String SENHA = "111111";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
